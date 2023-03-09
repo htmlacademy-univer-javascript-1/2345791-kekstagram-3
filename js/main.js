b2ec5155b8a91bb5bdaa5aaf71483fd970740aad
Damir <kim2.da@edu.spbstu.ru>
2023-03-09 18:38:38 +0300

    module4-task1

--- js/main.js
+++ js/main.js
@@ -7,3 +7,21 @@ let getRandom = function(min, max) {
 let checkLength = function(string, maxLength) {
   return string.length <= maxLength;
 }
+let getPhotos = function() {
+  let photos = [];
+  for (let i  = 0; i <= 24; i++) {
+    photos[i] = {
+      id:i+1,
+      url: `photos/${i+1}.jpg`,
+      description: `фото под номером ${i+1}`,
+      likes: getRandom(15, 200),
+      comments: getRandom(0, 200)
+    };
+  }
+  return photos;
+}
+let photos = getPhotos();
+for (let i  = 0; i < photos.length; i++) {
+  console.table(photos[i]);
+}
+
