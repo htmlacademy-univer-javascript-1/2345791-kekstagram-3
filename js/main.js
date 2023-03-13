5a6aee3412d91602da3cf8bdd97e76cbbc2bf3ba
Damir <kim2.da@edu.spbstu.ru>
2023-03-13 18:28:03 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const getRandom = function(min, max) {
   if (max < min) {
-    throw new Error("The number range is incorrect. getRandom(min, max), where min <= max");
-  }
+    throw new Error('The number range is incorrect. getRandom(min, max), where min <= max');
+  };
   return Math.round(Math.random() * (max-min) + min);
 };
 const checkLength = function(string, maxLength) {
@@ -19,5 +19,5 @@ const getPhotos = function() {
     });
   };
   return photos;
-}
-
+};
+getPhotos();
